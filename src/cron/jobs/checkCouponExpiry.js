f1210d7f6d136a22bc9f8d267dc786a03f5f16b4
purwar1997 <shubhampurwar511@gmail.com>
2024-08-15 03:17:49 +0530

    feat: setup pm2 to keep the app running

--- src/cron/jobs/checkCouponExpiry.js
+++ src/cron/jobs/checkCouponExpiry.js
@@ -1,8 +1,11 @@
 import Coupon from '../../models/coupon.js';
+import connectToDB from '../../db/index.js';
 import { COUPON_STATUS } from '../../constants/common.js';
 
 export const checkCouponExpiry = async () => {
   try {
+    connectToDB();
+
     await Coupon.updateMany(
       {
         expiryDate: { $lt: new Date() },
@@ -13,7 +16,7 @@ export const checkCouponExpiry = async () => {
       }
     );
   } catch (error) {
-    console.log('Error checking expiry date of coupons');
-    console.error(error);
+    console.log('Failed to check expiry date of coupons');
+    console.error('ERROR:', error);
   }
 };
