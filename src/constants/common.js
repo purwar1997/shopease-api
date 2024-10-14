cfe64c76d5e35d6981d5dff2c1c390ffdd5b159d
purwar1997 <shubhampurwar511@gmail.com>
2024-10-14 18:30:16 +0530

    Update URL of a website logo

--- src/constants/common.js
+++ src/constants/common.js
@@ -132,7 +132,7 @@ export const CRON_OPTIONS = Object.freeze({
 export const BUSINESS_INFO = Object.freeze({
   NAME: 'Shopease',
   WEBSITE_LOGO:
-    'https://res.cloudinary.com/dlqnx5pot/image/upload/v1724768966/shopease-logo_a9ayl7.svg',
+    'https://res.cloudinary.com/dlqnx5pot/image/upload/v1728910430/shopease-logo_xnwgk6.png',
   WEBSITE_URL: 'http://shopease.shubhampurwar.in',
   CONTACT_EMAIL: 'shubhampurwar35@gmail.com',
   CONTACT_PHONE: '9897887871',
